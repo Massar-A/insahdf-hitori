36c0d3c5499e5b1ca871669f674047f2f762feb7
Massar <massarabbas62@gmail.com>
2023-09-25 15:13:49 +0200

    almost finished menu

--- hitori/lib/main.dart
+++ hitori/lib/main.dart
@@ -72,16 +72,18 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> loadPreferences() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
-      gridSize = (prefs.getString("gridSize") != null ? listOfGrid[prefs.getString("gridSize")] : 5)!;
-      stringGridSize = (prefs.getString("gridSize") ?? "5x5")!;
+      gridSize = (prefs.getString("gridSize") != null
+          ? listOfGrid[prefs.getString("gridSize")]
+          : 5)!;
+      stringGridSize = prefs.getString("gridSize") ?? "5x5";
     });
   }
 
-  void setNewDefaultGridSize(value) async {
+  void _setNewDefaultGridSize(value) async {
     final prefs = await SharedPreferences.getInstance();
-        setState(() {
-          prefs.setString("gridSize", value);
-        });
+    setState(() {
+      prefs.setString("gridSize", value);
+    });
   }
 
   @override
@@ -89,102 +91,126 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
+          children: [
             const Padding(
-              padding: EdgeInsets.only(top: 80.0, bottom: 120),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hitori",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Libre',
-                      fontSize: 60,
+                padding: EdgeInsets.only(top: 80, bottom: 150),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hitori",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Libre',
+                            fontSize: 60,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ),
-            if (showContinueButton) // Affiche le bouton "Continuer la partie" si showContinueButton est true
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Action à exécuter pour le bouton "Continuer la partie"
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(300, 50),
-                  ),
-                  child: const Text(
-                    "Continuer la partie",
-                    style: TextStyle(fontSize: 22),
-                  ),
-                ),
-              ),
-            Center(
-              child: SizedBox(
-                width: 300.0, // Définissez la largeur souhaitée
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  // Centrer horizontalement
-                  children: [
-                    const Padding(
-                        padding: EdgeInsets.only(right: 20), child: Text(
-                      "Taille de la grille",
-                      style: TextStyle(fontSize: 20,
-                          fontFamily: 'Libre', fontStyle: FontStyle.italic),
-                    )),
-                    DropdownButton<int>(
-                      value: gridSize,
-                      onChanged: (int? selectedSize) {
-                        if (selectedSize != null) {
-                          setState(() {
-                            gridSize = selectedSize;
-                            setNewDefaultGridSize("${selectedSize}x$selectedSize");
-                          });
-                        }
-                      },
-                      items: listOfGrid.entries.map((MapEntry<String, int> entry) {
-                        return DropdownMenuItem<int>(
-                          value: entry.value,
-                          child: Text(entry.key),
-                        );
-                      }).toList(),
-                    ),
-                    Text("$gridSize")
                   ],
+                )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Visibility(
+                  visible: showContinueButton,
+                  // Affiche le bouton "Continuer la partie" si showContinueButton est true
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 50),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Action à exécuter pour le bouton "Continuer la partie"
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(300, 48),
+                      ),
+                      child: const Text(
+                        "Continuer la partie",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: ElevatedButton(
-                onPressed: _incrementCounter,
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(300, 50),
+                Visibility(
+                  visible: !showContinueButton,
+                  // Affiche le bouton "Continuer la partie" si showContinueButton est true
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 50),
+                    child: Container(
+                      height: 48,
+                    ),
+                  ),
                 ),
-                child: const Text(
-                  "▶️ Nouvelle Partie",
-                  style: TextStyle(fontSize: 22),
+                Center(
+                  child: SizedBox(
+                    width: 350, // Définissez la largeur souhaitée
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // Centrer horizontalement
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: ElevatedButton(
+                            onPressed: _incrementCounter,
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: const Size(220, 48),
+                            ),
+                            child: const Row(
+                              children: [
+                                Icon(Icons.play_arrow_rounded),
+                                Text(
+                                  "Nouvelle Partie",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        DropdownButton<int>(
+                          value: gridSize,
+                          onChanged: (int? selectedSize) {
+                            if (selectedSize != null) {
+                              setState(() {
+                                gridSize = selectedSize;
+                                _setNewDefaultGridSize(
+                                    "${selectedSize}x$selectedSize");
+                              });
+                            }
+                          },
+                          items: listOfGrid.entries
+                              .map((MapEntry<String, int> entry) {
+                            return DropdownMenuItem<int>(
+                              value: entry.value,
+                              child: Text(entry.key),
+                            );
+                          }).toList(),
+                          style: const TextStyle(
+                              fontSize: 18, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: ElevatedButton(
-                onPressed: _turnOnOffButton,
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(300, 50),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: ElevatedButton(
+                    onPressed: _turnOnOffButton,
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(300, 50),
+                    ),
+                    child: const Text(
+                      "Allumer/Éteindre bouton continuer",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
                 ),
-                child: const Text(
-                  "Allumer/Éteindre bouton continuer",
-                  style: TextStyle(fontSize: 25),
-                ),
-              ),
-            ),
+              ],
+            )
           ],
         ),
       ),
